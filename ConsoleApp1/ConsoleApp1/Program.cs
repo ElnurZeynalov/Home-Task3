60d81f879545d54093b5b9ae06fecaec685e79a5
ElnurZeynalov <tu74449ce@code.edu.az>
2022-03-07 22:02:18 +0400

    Task 1 Complete

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.Write("Herf axtaricalaq cumleni daxil edin: ");
+            string x = Console.ReadLine();
+            Console.Write("axtarilacaq herfi daxil edin: ");
+            char y = Convert.ToChar(Console.ReadLine());
+            Search(x,y);
+        }
+        static void Search(string cumle, char herf , int total=0)
+        {
+            for (int i = 0; i < cumle.Length; i++)
+            {
+                if (cumle[i] == herf)
+                    total++;
+                
+            }
+            if(total != 0)
+                Console.WriteLine("Bu sozde "+ total +" eded " + herf +" herfi var");
+            else
+            Console.WriteLine("Bu sozde " + herf + " herfi yoxdur!" );
+        }
+    }
+}
