3ecd04b24193cb970e5d134bf42204eb31bdc3ee
ElnurZeynalov <tu74449ce@code.edu.az>
2022-03-08 13:28:00 +0400

    Task 2 Complete and Changed

--- ConsoleApp2/ConsoleApp2/Program.cs
+++ ConsoleApp2/ConsoleApp2/Program.cs
@@ -8,35 +8,34 @@ namespace ConsoleApp2
         {
             Console.WriteLine("2 Ci taskin sertini tam basa dusmedim ona gore ikisinde yazdim :)");
             Console.Write("1) Array Uzunlugunu daxil edin: ");
-            int n =Convert.ToInt32(Console.ReadLine());
-            int[] z = new int[n];
-            for (int i = 0; i < n; i++)
+            int Arr_Length =Convert.ToInt32(Console.ReadLine());
+            int[] Array = new int[Arr_Length];
+            for (int i = 0; i < Arr_Length; i++)
             {
                 Console.Write(i+ ".Arrayin reqemleri daxil edin: ");
-                z[i] = Convert.ToInt32(Console.ReadLine());
+                Array[i] = Convert.ToInt32(Console.ReadLine());
             }
             Console.Write("2) Hesablanacaq Reqemi daxil edin: ");
-            int y = Convert.ToInt32(Console.ReadLine());
-            Arr_Sum(z); 
-            Num_Sum(y);
-            //Ozum bilerek Z yazdim )
+            int Number = Convert.ToInt32(Console.ReadLine());
+            Arr_Sum(Array); 
+            Num_Sum(Number);
 
         }
-        static void Arr_Sum(int[] a , int sum = 0)
+        static void Arr_Sum(int[] Array , int sum = 0)
         {
-            for (int i = 0; i < a.Length; i++)
+            for (int i = 0; i < Array.Length; i++)
             {
-                sum = sum + a[i];
+                sum = sum + Array[i];
             }
             Console.WriteLine("Arrayin reqemlerinin cemi: "+sum);
         }
-        static void Num_Sum(int y , int sum = 0)
+        static void Num_Sum(int Number , int sum = 0)
         {
-            for (int i = 0; i <= y; i++)
+            for (int i = 0; i <= Number; i++)
             {
                 sum = sum + i;
             }
-            Console.WriteLine(y + " reqeme qeder olan reqemlerin cemi: " + sum);
+            Console.WriteLine(Number + " reqeme qeder olan reqemlerin cemi: " + sum);
         }
 
     }
